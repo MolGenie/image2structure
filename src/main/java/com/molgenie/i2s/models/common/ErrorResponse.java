d746135fdd01c90374382ba50edb145c41b1a49d
Lutz Weber <lutz.weber@molgenie.com>
2025-07-28 15:27:01 +0200

    changed Error

--- src/main/java/com/molgenie/i2s/models/common/ErrorResponse.java
+++ src/main/java/com/molgenie/i2s/models/common/ErrorResponse.java
@@ -3,7 +3,7 @@ package com.molgenie.i2s.models.common;
 public record ErrorResponse(ProcessState processState) {
 
     public ErrorResponse(String error, int errorCode, String fileName) {
-        this(new ProcessState(false, errorCode, error));
+        this( new ProcessState( false, errorCode, error) );
     }
     
 }
